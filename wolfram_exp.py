64554001a8b495c41c8bbe1a68c93b6fef21235c
TroyeJames9 <130838331+TroyeJames9@users.noreply.github.com>
2024-03-26 11:03:01 +0800

    init

--- wolfram_exp.py
+++ wolfram_exp.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from wolframclient.evaluation import WolframLanguageSession
+from wolframclient.language import wl, wlexpr
+
+
+# Replace 'your_kernel_path' with the actual path to the Wolfram Kernel executable.
+# exp: session = WolframLanguageSession(r'your_kernel_path')
+# you should have download mathematica or wolfram engine
+# how to find Wolfram Kernel executable: "mathematica/MathKernel.exe" or "Wolfram Engine\*\MathKernel.exe"
+# like this↓
+session = WolframLanguageSession(r'D:\mathematica\MathKernel.exe')
+
+# Evaluate any Wolfram Language code from Python
+print(session.evaluate(wlexpr('Range[5]')))
+print(session.evaluate(wl.MinMax([1, -3, 0, 9, 5])))
+
+session.terminate()
